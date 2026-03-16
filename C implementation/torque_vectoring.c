d9dd6f28a8d4c72b87b4c150ed398e825f9f42ec
Rolando Giugliano <roly22216@gmail.com>
2026-03-16 09:20:03 +0000

    first C implementation

---
+++ C implementation/torque_vectoring.c
@@ -0,0 +1,34 @@
+#include "torque_vectoring.h"
+
+float TV_PID(float yaw_rate_ref, float yaw_rate_actual, PID_State *pid) {
+    // Calculate error
+    float error = yaw_rate_ref - yaw_rate_actual;
+
+    // Proportional term
+    float P = pid->Kp * error;
+
+    // Anti - wind up for integral term
+
+    float Mz_test = P + pid->integral_acc + (pid->Ki * error * pid->dt);
+
+    if ( (Mz_test >= MZ_MAX && error > 0.0f) ) || (Mz_test <= -MZ_MAX && error < 0.0f) ) {
+        // Do not update integral accumulator to prevent wind-up
+    } else {
+        // Update integral accumulator
+        pid->integral_acc += pid->Ki * error * pid->dt;
+    }
+
+    // Derivative term
+
+    float D = 0.0f; // Derivative term is set to zero
+
+    // Mz control output
+
+    float Mz_out = P + pid->integral_acc + D;
+
+    if (Mz_out > MZ_MAX) { Mz_out = MZ_MAX; }
+    if (Mz_out < -MZ_MAX) { Mz_out = -MZ_MAX; }
+
+    return Mz_out;
+}
+
---
+++ C implementation/torque_vectoring.h
@@ -0,0 +1,35 @@
+#ifndef TORQUE_VECTORING_H
+#define TORQUE_VECTORING_H
+
+// Vehicle parameters
+
+#define T_DRIVE_MAX     400.0f  // Maximum drive torque (Nm)
+#define T_REGEN_MAX     25.0f   // Maximum regenerative torque (Nm)
+#define TRACK_WIDTH     1.136f  // Track width vp.t2 (m)
+#define WHEEL_RADIUS    0.2395f // Wheel radius vp.Rr (m)
+
+// Control limits
+
+#define MZ_MAX          1300.0f  // Maximum yaw moment (Nm)
+
+// Control structs
+
+typedef struct {
+    float Kp;  // Proportional gain
+    float Ki;  // Integral gain
+    float Kd;  // Derivative gain
+    float integral_acc; // Integral accumulator
+    float dt; // Sample time (s)
+} PID_State;
+
+typedef struct {
+    float T_left;   // Torque command for left wheel (Nm)
+    float T_right;  // Torque command for right wheel (Nm)
+} Wheel_Torques;
+
+// Function prototypes
+
+float TV_PID(float yaw_rate_ref, float yaw_rate_actual, PID_State *pid);
+Wheel_Torques torque_allocator(float T_req_pilot, float Mz_ctrl);
+
+#endif
